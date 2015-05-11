57fa19118d220a3440a83e89c02b6e9ec76cb7d5
dbuck <dbuck008@gmail.com>
2015-05-11 13:06:36 -0400

    Works with 320p

--- app/src/main/java/net/dbuck/twowaystreaming/MainActivity.java
+++ app/src/main/java/net/dbuck/twowaystreaming/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends Activity {
     private final static String TAG = "MainActivity";
 
     private SurfaceView mSurfaceView;
+    private VideoQuality videoQuality = new VideoQuality(320,240,20,500000);
+    private SharedPreferences settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +32,28 @@ public class MainActivity extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         mSurfaceView = (SurfaceView) findViewById(R.id.surface);
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
 
         // Sets the port of the RTSP server to 1234
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
         editor.putString(RtspServer.KEY_PORT, String.valueOf(8086));
         editor.apply();
 
+        // Read video quality settings from the preferences
+        videoQuality = new VideoQuality(
+                settings.getInt("video_resX", videoQuality.resX),
+                settings.getInt("video_resY", videoQuality.resY),
+                Integer.parseInt(settings.getString("video_framerate", String.valueOf(videoQuality.framerate))),
+                Integer.parseInt(settings.getString("video_bitrate", String.valueOf(videoQuality.bitrate/1000)))*1000);
+
+
         // Configures the SessionBuilder
         SessionBuilder.getInstance()
                 .setSurfaceView(mSurfaceView)
-                .setPreviewOrientation(90)
+                .setPreviewOrientation(0)
                 .setContext(getApplicationContext())
                 .setAudioEncoder(SessionBuilder.AUDIO_NONE)
-                .setVideoQuality(new VideoQuality(320, 240, 30, 500000))
+                .setVideoQuality(videoQuality)
                 .setVideoEncoder(SessionBuilder.VIDEO_H264);
 
         // Starts the RTSP server
@@ -72,4 +83,22 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private SharedPreferences.OnSharedPreferenceChangeListener mOnSharedPreferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (key.equals("video_resX") || key.equals("video_resY")) {
+                videoQuality.resX = sharedPreferences.getInt("video_resX", 0);
+                videoQuality.resY = sharedPreferences.getInt("video_resY", 0);
+            }
+
+            else if (key.equals("video_framerate")) {
+                videoQuality.framerate = Integer.parseInt(sharedPreferences.getString("video_framerate", "0"));
+            }
+
+            else if (key.equals("video_bitrate")) {
+                videoQuality.bitrate = Integer.parseInt(sharedPreferences.getString("video_bitrate", "0"))*1000;
+            }
+        }
+    };
 }
--- app/src/main/java/net/dbuck/twowaystreaming/Settings.java
+++ app/src/main/java/net/dbuck/twowaystreaming/Settings.java
@@ -22,6 +22,7 @@ public class Settings extends PreferenceActivity implements View.OnClickListener
     EditText port, resolutionX, resolutionY, bitrate, framerate;
     Button apply;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,10 +43,10 @@ public class Settings extends PreferenceActivity implements View.OnClickListener
         apply.setOnClickListener(this);
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        resolutionX.setText(prefs.getString("ResolutionX", "176"));
-        resolutionY.setText(prefs.getString("ResolutionY", "144"));
-        bitrate.setText(prefs.getString("Bitrate", "30"));
-        framerate.setText(prefs.getString("Framerate", "500000"));
+        resolutionX.setText(prefs.getString("video_resX", "176"));
+        resolutionY.setText(prefs.getString("video_resY", "144"));
+        framerate.setText(prefs.getString("video_framerate", "30"));
+        bitrate.setText(prefs.getString("video_bitrate", "500000"));
 
     }
 
@@ -54,21 +55,12 @@ public class Settings extends PreferenceActivity implements View.OnClickListener
         if(view.getId() == R.id.apply){
             SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
             editor.putString(RtspServer.KEY_PORT, String.valueOf(Integer.parseInt(port.getText().toString()))); // save port
-            editor.putString("ResolutionX", resolutionX.getText().toString()); // save resolution
-            editor.putString("ResolutionY", resolutionY.getText().toString()); // save resolution
-            editor.putString("Bitrate", bitrate.getText().toString()); // save port
-            editor.putString("Framerate", framerate.getText().toString()); // save port
+            editor.putString("video_resX", resolutionX.getText().toString()); // save resolution
+            editor.putString("video_resY", resolutionY.getText().toString()); // save resolution
+            editor.putString("video_framerate", framerate.getText().toString()); // save framerate
+            editor.putString("video_bitrate", bitrate.getText().toString()); // save bitrate
 
             editor.apply();
-
-            // Configures the SessionBuilder
-            SessionBuilder.getInstance()
-                    .setVideoQuality(new VideoQuality(
-                            Integer.parseInt(resolutionX.getText().toString()),
-                            Integer.parseInt(resolutionY.getText().toString()),
-                            Integer.parseInt(framerate.getText().toString()),
-                            Integer.parseInt(bitrate.getText().toString())
-                    ));
         }
     }
 }
